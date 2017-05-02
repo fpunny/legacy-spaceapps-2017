01a98de8e6f6f2dfb979c1a03bd61be8c09e9445
Mohammad Ismail <mohammadn.ismail@mail.utoronto.ca>
2017-05-02 02:57:27 -0400

    Create Directory for .py files and placeholder
    - Directory Created for Python Algorithms Developed over the weekend at Spaceapps Toronto 2017
    - placeholder file sample_python_script.py added (to be replaced by actual .py files by Asonance)

---
+++ Python Image Processing and CSV generating Algorithms -To generate CSVs to use instead of sample data/sample_python_script.py
@@ -0,0 +1,8 @@
+# This python file is just a placeholder for the actual Image Processing and CSV generating Algorithms devloped
+# over the weekend at Spaceapps Toronto 2017
+
+def hello_world():
+  print("Hello World")
+ 
+if __name__ == "__main__":
+  hello_world()
